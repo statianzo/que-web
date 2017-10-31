statianzo/que-web

commit 38597ba46b0279a6fd144508ada962cb4a46c739

Fix #29: Ensure advisory locks are released when updating/deleting jobs (#30)

lib/que/web/sql.rb
@@ -43,15 +43,15 @@ Que::Web::SQL = {
     DELETE
     FROM que_jobs
     WHERE job_id = $1::bigint
-    AND pg_try_advisory_lock($1::bigint)
-    RETURNING job_id
+    AND pg_try_advisory_lock(job_id)
+    RETURNING pg_advisory_unlock(job_id)
   SQL
   reschedule_job: <<-SQL.freeze,
     UPDATE que_jobs
     SET run_at = $2::timestamptz
     WHERE job_id = $1::bigint
-    AND pg_try_advisory_lock($1::bigint)
-    RETURNING job_id
+    AND pg_try_advisory_lock(job_id)
+    RETURNING pg_advisory_unlock(job_id)
   SQL
   fetch_job: <<-SQL.freeze,
     SELECT *

commit a380d38dee7620e788376839776e9b1de782d65c

Fix regression introduced in #30 that causes all enqueued jobs to be locked when one is deleted or re-scheduled.

lib/que/web/sql.rb
@@ -43,14 +43,14 @@ Que::Web::SQL = {
     DELETE
     FROM que_jobs
     WHERE job_id = $1::bigint
-    AND pg_try_advisory_lock(job_id)
+    AND pg_try_advisory_lock($1::bigint)
     RETURNING pg_advisory_unlock(job_id)
   SQL
   reschedule_job: <<-SQL.freeze,
     UPDATE que_jobs
     SET run_at = $2::timestamptz
     WHERE job_id = $1::bigint
-    AND pg_try_advisory_lock(job_id)
+    AND pg_try_advisory_lock($1::bigint)
     RETURNING pg_advisory_unlock(job_id)
   SQL
   fetch_job: <<-SQL.freeze,