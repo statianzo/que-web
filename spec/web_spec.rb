6f10b3116dd4a38441077602d3bb64faef20228e
Owen Stephens <owen@owenstephens.co.uk>
2018-05-31 18:48:41 +0100

    Fix #41 - use hash lookup rather than method call for job_class (#42)
    Calling `Que.worker_states` returns an array of Hashes rather than
    objects that directly respond to `#job_class`. Also remove usage of `#present?` to eliminate implicit dependency on activesupport

--- spec/web_spec.rb
+++ spec/web_spec.rb
@@ -1,8 +1,18 @@
 require 'spec_helper'
 
 describe Que::Web::Helpers do
+  let(:search) { '' }
+  let(:params) { { 'search' => search } }
+
   subject do
-    Class.new { include Que::Web::Helpers }.new
+    # Capture the params into an ivar so the params method block below captures it
+    params_to_use = params
+
+    Class.new do
+      include Que::Web::Helpers
+
+      define_method(:params) { params_to_use }
+    end.new
   end
 
   def error_job(last_error)
@@ -20,6 +30,46 @@ describe Que::Web::Helpers do
 
     it 'handles empty strings as the last error' do
       subject.format_error(error_job('')).must_equal ''
+    end
+  end
+
+  describe '#search_running' do
+    let(:jobs) do
+      [
+        { 'job_class' => 'JobClassA' },
+        { 'job_class' => 'JobClassB' },
+        { 'job_class' => 'JobClassA2' },
+        { 'job_class' => 'JobClassC' }
+      ]
+    end
+
+    describe 'when the search param is not supplied' do
+      let(:params) { {} }
+
+      it 'returns all the jobs' do
+        subject.search_running(jobs).must_equal(jobs)
+      end
+    end
+
+    describe 'when the search param is blank' do
+      let(:search) { '' }
+
+      it 'returns all the jobs' do
+        subject.search_running(jobs).must_equal(jobs)
+      end
+    end
+
+    describe 'when the search param is present' do
+      let(:search) { 'A' }
+
+      it 'returns only the jobs whose class matches the search' do
+        subject.search_running(jobs).must_equal(
+          [
+            { 'job_class' => 'JobClassA' },
+            { 'job_class' => 'JobClassA2' }
+          ]
+        )
+      end
     end
   end
 end
