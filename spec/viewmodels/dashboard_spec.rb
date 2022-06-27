d8efdd07f5928734613ca2257836c69760cfd8fa
Gerry Power <gerry.power@homestars.com>
2022-06-27 19:31:30 -0400

    Updated with additional attributes

--- spec/viewmodels/dashboard_spec.rb
+++ spec/viewmodels/dashboard_spec.rb
@@ -3,15 +3,37 @@ require "spec_helper"
 describe Que::Web::Viewmodels::Dashboard do
   let(:dashboard_stats) {
     {
-      total: 10,
       running: 6,
-      failing: 2,
-      scheduled: 2
+      queued: 2,
+      scheduled: 3,
+      failing: 4,
+      errored: 5,
+      failed: 6
     }
   }
   let(:subject) { Que::Web::Viewmodels::Dashboard.new(dashboard_stats) }
 
-  it 'passes through values' do
-    _(subject.scheduled).must_equal 2
+  it 'passes through running' do
+    _(subject.running).must_equal 6
+  end
+
+  it 'passes through queued' do
+    _(subject.queued).must_equal 2
+  end
+
+  it 'passes through scheduled' do
+    _(subject.scheduled).must_equal 3
+  end
+
+  it 'passes through failing' do
+    _(subject.failing).must_equal 4
+  end
+
+  it 'passes through errored' do
+    _(subject.errored).must_equal 5
+  end
+
+  it 'passes through failed' do
+    _(subject.failed).must_equal 6
   end
 end
